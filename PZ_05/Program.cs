dce25c7c289efb810b807c56decd5ad1be7f2a6d
Александра Тамбовцева <228884rus@mail.ru>
2023-09-23 00:26:19 +0500

    Добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("Число  Квадрат  Куб");
+
+            do
+            {
+                double a = Math.Pow(i, 2);
+                double b = Math.Pow(i, 3);
+                Console.WriteLine($"{i,-7} {a,-7} {b}");
+                i++;
+            }
+            while (i <= 10);
+
+            Console.ReadLine();
+        }
+    }
+}
